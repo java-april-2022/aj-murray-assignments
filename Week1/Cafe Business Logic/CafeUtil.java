bb5df798c0394c26f3a79823e27da78c2c72e53c
AJ-Murray <ajmurraysd@gmail.com>
2022-04-10 17:19:11 -0600

    First commit

---
+++ Week1/Cafe Business Logic/CafeUtil.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+
+public class CafeUtil {
+    //Get Streak Goal
+    public int getStreakGoal(){
+        int sum = 0;
+        for(int week = 1; week <= 10; week++){
+            sum+= week;
+        }
+        return sum;
+    }
+
+    //Get Order Total
+    public double getOrderTotal(double[] lineItems){
+        double sum = 0;
+        for (double price: lineItems) {
+            sum += price;
+        
+        }
+        return sum;
+    }
+
+    //Add Customer
+    public void addCustomer(ArrayList<String> customerList){
+            System.out.println("Please enter your name:");
+            String userName = System.console().readLine();
+            System.out.printf("Hello, %s! ", userName);
+            System.out.printf("There are %s people ahead of you.\n", customerList.size());
+            customerList.add(userName);
+            System.out.println(customerList);
+        }
+
+        //Display Menu
+    public void displayMenu(ArrayList<String> menuItems) {
+        for (int id = 0; id < menuItems.size(); id++) {
+            System.out.printf("%s %s \n", id, menuItems.get(id));
+        }
+    }
+
+}
